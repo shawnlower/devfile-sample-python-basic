de0a6a22c984b982de047d1efd424d8a66d93492
Shawn Lower <shawn@shawnlower.com>
2024-05-25 16:31:49 +0000

    add postgres fn

--- app.py
+++ app.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/db')
+def db():
+    conn = psycopg2.connect(host='DB',
+                            database='flask_db',
+                            user=os.environ['pguser'],
+                            password=os.environ['pgpass'])
+
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema = 'public'
+    """)
+    table_names = cur.fetchall()
+    return "".join("<p>Table name: {}</p>".format(t) for t in table_names)
+
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
